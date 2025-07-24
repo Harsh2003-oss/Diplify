b59fe70f1c7d391cb415b597217081a2be78160f
Harsh2003-oss <harsh.sil872@gmail.com>
2025-07-24 13:59:14 +0530

    changes

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -6,17 +6,21 @@ const LandingPage = () => {
         <>
             <div className='w-full h-screen bg-zinc-900 pt-1'>
                 <div className='textstructure mt-52 px-20'>
-                  {["We Create","Eye Opening","Presentations"].map((items,index)=>{
-                    return   <div className='masker text-[7.5vw] uppercase leading-[6.3vw] tracking-tight font-medium'>
+                  {["We","Create","Wardrobe Magic",].map((items,index)=>{
+                    return   <div className='masker text-[7.5vw] uppercase leading-[6vw] tracking-tighter font-medium'>
                         <h1>{items}</h1>
                         </div>
                   })}
                     
                 </div>
-                <div className='border-t-[1px] border-zinc-800 mt-32 py-5 px-20'>
+                <div className='border-t-[1px] border-zinc-800 mt-32 py-5 px-20 flex justify-between'>
 {["For public and private companies","From the first pitch to IPO"].map((item,index)=>{
     return <p className='text-md font-light tracking-tight leading-none'>{item}</p>
 })}
+
+<div className='start'>
+    <div className='px-2 py-.5 mb-5 uppercase border-[1px] border-zinc-500 font-light text-sm rounded-xl'>Explore</div>
+</div>
                 </div>
                 </div>
           
